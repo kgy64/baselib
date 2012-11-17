e1cd673e7c24d1b054216620059df0a973da2097
Gyorgy Kovesdi <kgy@teledigit.eu>
2012-11-17 21:52:00 +0100

    More detailed main page

--- include/doc.h
+++ include/doc.h
@@ -1,6 +1,12 @@
 /*!
     \mainpage My Base Library Documentation
-    The source can be downloaded from git.teledigit.eu:/git/lib/base.git
+    The source can be downloaded from <b>git.teledigit.eu:/git/lib/base.git</b><br>
+    For example you can use the following code to obtain the source:
+    \code
+ git clone git.teledigit.eu:/git/lib/base.git
+    \endcode
+    Note that this is a read-only access.
+    <hr>
  */
  \
 /* * * * * * * * * * * * * End - of - File * * * * * * * * * * * * * * */
