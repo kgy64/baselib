e37068d60eaf731da3e7d8b9ba8a874754b92be8
Gyorgy Kovesdi <kgy@teledigit.eu>
2013-01-10 19:04:10 +0100

    Priority.h is deleted, its content is moved to Generic.h

--- opsys/unix/System/Generic.h
+++ opsys/unix/System/Generic.h
@@ -11,9 +11,20 @@
 #ifndef __OPSYS_UNIX_SYSTEM_GENERIC_H_INCLUDED__
 #define __OPSYS_UNIX_SYSTEM_GENERIC_H_INCLUDED__
 
-#define PACKED  __attribute__(( aligned(1), packed ))
+#define PACKED  __attribute__((aligned(1), packed))
 
+/// Forces to the object to be linked in
 #define MAKE_REFERENCED __attribute__((used))
+
+/// Increases the static initialization priority
+/*! Such a variable will be initialized before others.
+    \note   It is useful only for statically initialized instances. */
+#define INITIALIZE_PRIORITY_HIGH    __attribute__((init_priority(500)))
+
+/// Decreases the static initialization priority
+/*! Such a variable will be initialized after others.
+    \note   It is useful only for statically initialized instances. */
+#define INITIALIZE_PRIORITY_LOW     __attribute__((init_priority(1500)))
 
 #endif /* __OPSYS_UNIX_SYSTEM_GENERIC_H_INCLUDED__ */
 
